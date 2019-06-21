63d0926d6f50205ac5a38687388ec9e758e84bbe
jpmchatton98 <mch18001@byui.edu>
2019-06-21 11:22:41 -0600

    Added functionality for button to bring up the pop-up window for adding items to the list.

--- app/src/main/java/com/example/projectbakery/MainActivity.java
+++ app/src/main/java/com/example/projectbakery/MainActivity.java
@@ -4,7 +4,11 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 import java.util.ArrayList;
 
@@ -12,31 +16,46 @@ import static junit.framework.Assert.*; //Useless now, wil be used for unit test
 
 public class MainActivity extends AppCompatActivity
 {
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 
-    public void saveStorage(View view)
-	 {
-	 		SaveAndLoad saving = new SaveAndLoad();
+	public void saveStorage(View view)
+	{
+		SaveAndLoad saving = new SaveAndLoad();
 
-	 		ArrayList<InventoryItem> testItems = new ArrayList<>();
-	 		testItems.add(new InventoryItem("A", "A", 5));
-	 		testItems.add(new InventoryItem("B", "A", 7));
-	 		testItems.add(new InventoryItem("C", "B", 4));
+		ArrayList<InventoryItem> testItems = new ArrayList<>();
+		testItems.add(new InventoryItem("A", "A", 5));
+		testItems.add(new InventoryItem("B", "A", 7));
+		testItems.add(new InventoryItem("C", "B", 4));
 
-	 		Context context = this;
-		 SharedPreferences pref = context.getSharedPreferences("com.example.projectbakery", Context.MODE_PRIVATE);
-	 		saving.saveToFile(testItems, pref);
-	 }
-	 public void loadStorage(View view)
-	 {
-		 Context context = this;
-		 SharedPreferences pref = context.getSharedPreferences("com.example.projectbakery", Context.MODE_PRIVATE);
+		Context context = this;
+		SharedPreferences pref = context.getSharedPreferences("com.example.projectbakery", Context.MODE_PRIVATE);
+		saving.saveToFile(testItems, pref);
+	}
 
-		 System.out.println(pref.getString("masterStorage", ""));
-	 }
+	public void loadStorage(View view)
+	{
+		Context context = this;
+		SharedPreferences pref = context.getSharedPreferences("com.example.projectbakery", Context.MODE_PRIVATE);
+
+		System.out.println(pref.getString("masterStorage", ""));
+	}
+
+	public void addItem(View view)
+	{
+	 	LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	 	View popupView = inflater.inflate(R.layout.item_input_popup, null);
+
+	 	int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+	 	int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+
+	 	boolean focusable = true;
+	 	final PopupWindow window = new PopupWindow(popupView, width, height, focusable);
+
+	 	window.showAtLocation(view, Gravity.CENTER, 0, 0);
+	}
 }
