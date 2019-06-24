96ccb1b63741371a61a8a5c069ed4521a38bf8e5
jpmchatton98 <mch18001@byui.edu>
2019-06-24 12:24:47 -0600

    Custom adapter modified.

--- app/src/main/java/com/example/projectbakery/StorageListAdapter.java
+++ app/src/main/java/com/example/projectbakery/StorageListAdapter.java
@@ -4,19 +4,29 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ListAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class StorageListAdapter extends BaseAdapter implements ListAdapter
+public class StorageListAdapter extends ArrayAdapter<InventoryItem> implements ListAdapter
 {
+	class ViewHolder
+	{
+		TextView name;
+		TextView amount;
+		TextView category;
+	}
+
 	private ArrayList<InventoryItem> list = null;
 	private Context context;
 
 	StorageListAdapter(ArrayList<InventoryItem> list, Context context)
 	{
+		super(context, R.layout.list_item, list);
 		this.list = list;
 		this.context = context;
 	}
@@ -28,7 +38,7 @@ public class StorageListAdapter extends BaseAdapter implements ListAdapter
 	}
 
 	@Override
-	public Object getItem(int position)
+	public InventoryItem getItem(int position)
 	{
 		return list.get(position);
 	}
@@ -42,17 +52,25 @@ public class StorageListAdapter extends BaseAdapter implements ListAdapter
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent)
 	{
-		View view = convertView;
-		if(view == null)
-		{
-			LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			view = inflater.inflate(R.layout.list_item, null);
-		}
+		InventoryItem item = getItem(position);
 
-		TextView name = (TextView) view.findViewById(R.id.nameLabel);
-		TextView category = (TextView) view.findViewById(R.id.categoryLabel);
-		TextView amount = (TextView) view.findViewById(R.id.amountLabel);
+		ViewHolder holder;
 
-		return view;
+		holder = new ViewHolder();
+
+		LayoutInflater inflater = LayoutInflater.from(getContext());
+		convertView = inflater.inflate(R.layout.list_item, parent, false);
+
+		holder.name = convertView.findViewById(R.id.itemNameText);
+		holder.amount = convertView.findViewById(R.id.amountText);
+		holder.category = convertView.findViewById(R.id.categoryText);
+
+		convertView.setTag(holder);
+
+		holder.name.setText(item.getName());
+		holder.amount.setText(item.getAmount());
+		holder.category.setText(item.getCategory());
+
+		return convertView;
 	}
 }
