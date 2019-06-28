ac87993a2430c962cef4aa7ee83afc62ee5337d4
jpmchatton98 <mch18001@byui.edu>
2019-06-28 12:13:34 -0600

    Moved pop-up handling into MainActivity.java

--- app/src/main/java/com/example/projectbakery/MainActivity.java
+++ app/src/main/java/com/example/projectbakery/MainActivity.java
@@ -86,6 +86,7 @@ public class MainActivity extends AppCompatActivity
 			{
 				InventoryItem item = new InventoryItem(itemName, itemCategory, itemAmount);
 				builder.addItem(item);
+				builder.printList();
 			}
 		});
 	}
